8948799ecdd6c9f535a0c720117459c4925184bb
Alexandr <drasikov.al@yandex.ru>
2020-03-23 01:29:29 +0300

    убрал кирилицу в аннотациях

--- src/test/java/ru/autotests/ApiTest.java
+++ src/test/java/ru/autotests/ApiTest.java
@@ -8,52 +8,41 @@ import org.testng.annotations.Test;
 
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+@Epic("Minimum set of positive cases")
+@Feature("Checking the requests")
+public class ApiTest extends BaseSteps {
 
-@Epic("Минимальный набор позитивных кейсов\\Minimum set of positive cases")
-@Feature("Проверка запросов\\checking requests")
-public class ApiTest extends BaseSteps{
-    @TmsLink("http://tfs.infotecs.ru:8080/tfs/infotecstc1/PRG_SIES/R610%20SIES%20MC/_workitems?id=493996&_a=edit")
-    @Test(description = "Применить сетевые настройки Основной сценарий \\ To apply the network settings the Main scenario")
+    @TmsLink("http://........id=493996&_a=edit")
+    @Test(description = "To checking the GET request")
     public void testGet() {
         checkGetResponse();
     }
 
-    @Test()
+    @TmsLink("Test 493996&")
+    @Test(description = "Check the POST request")
     public void testPOST() {
-        long threadId = Thread.currentThread().getId();
-        LOG.info("Thread Post:" + threadId);
-        Map<String, String> data = new HashMap<>();
-        data.put("orderId", "2");
-        given()
-                .contentType("application/json")
-                .body(data)
-                .when().post("http://httpbin.org/post").then()
-                .statusCode(200)
-                .body("json.orderId", equalTo("2"));
+        checkPostRequest();
     }
 
 
-    @Test(dataProvider = "api")
+    @TmsLink("Test 493996&")
+    @Test(dataProvider = "api", description = "Check the POST and GET with parallel run and dataProvider")
     public void test(String method) {
         if (method.equals("post")) {
             Date date = new Date();
-              LOG.info("Начало " + method + " "+ new Timestamp(date.getTime()));
+            LOG.info("Начало " + method + " " + new Timestamp(date.getTime()));
             testPOST();
-            date=new Date();
-              LOG.info("Конец " + method + " "+ new Timestamp(date.getTime()));
+            date = new Date();
+            LOG.info("Конец " + method + " " + new Timestamp(date.getTime()));
             return;
         }
         if (method.equals("get")) {
             Date date = new Date();
-              LOG.info("Начало " + method + " " +new Timestamp(date.getTime()));
+            LOG.info("Начало " + method + " " + new Timestamp(date.getTime()));
             testGet();
-            date=new Date();
-              LOG.info("Конец " + method + " "+ new Timestamp(date.getTime()));
+            date = new Date();
+            LOG.info("Конец " + method + " " + new Timestamp(date.getTime()));
             return;
         }
     }
--- src/test/java/ru/autotests/BaseSteps.java
+++ src/test/java/ru/autotests/BaseSteps.java
@@ -4,7 +4,11 @@ import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * @author Alexandr.Drasikov
@@ -20,16 +24,30 @@ public class BaseSteps {
         System.out.println("Thread Get:" + threadId);
         Response response1 =
                 given()
-                        //.log().all()
+                        .log().all()
                         .when()
                         .get("http://httpbin.org/get")
                         .then()
-                        //.log().all()
+                        .log().all()
                         .statusCode(200)
-//                        .body("headers.Connection", equalTo("close"))
-//                        .body("args.a", equalTo("1"))
+                        .body("headers.Connection", equalTo("close"))
+                        .body("args.a", equalTo("1"))
                         .extract()
                         .response();
         LOG.info(response1.print());
+    }
+
+    @Step("Check POST request")
+    public void checkPostRequest(){
+        long threadId = Thread.currentThread().getId();
+        LOG.info("Thread Post:" + threadId);
+        Map<String, String> data = new HashMap<>();
+        data.put("orderId", "2");
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when().post("http://httpbin.org/post").then()
+                .statusCode(200)
+                .body("json.orderId", equalTo("2"));
     }
 }
